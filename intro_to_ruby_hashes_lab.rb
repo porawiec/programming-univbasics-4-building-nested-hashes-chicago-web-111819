fb1556c9968ed8875e238a5609bc1c919b380fba
Paul O <paul.orawiec@gmail.com>
2019-10-29 07:45:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ def monopoly_with_third_tier
 	  :railroads => {
 	    pieces: 4, 
 	    rent_in_dollars: { 
-	      one_RR: 25,
+	      one_piece_owned: 25,
 	      two_RR: 50,
 	      three_RR: 100,
 	      four_RR: 200
